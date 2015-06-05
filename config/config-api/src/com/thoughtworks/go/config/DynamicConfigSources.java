c8b04f894b97613dffc5c3e9f632a538aef86b8d
Tomasz Setkowski <tom@ai-traders.com>
2015-06-05 18:38:51 +0200

    missing remote configuration sources class

---
+++ config/config-api/src/com/thoughtworks/go/config/DynamicConfigSources.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.go.config;
+
+import com.thoughtworks.go.config.materials.ConfigurationMaterialConfig;
+import com.thoughtworks.go.domain.BaseCollection;
+
+/**
+ * List of remote configuration sources and how to interpret them
+ */
+public class DynamicConfigSources extends BaseCollection<ConfigurationMaterialConfig> {
+
+}
